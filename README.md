188762a4932aacb67857da31f8a2835169402338
pdcamilleri <pdc.cse@gmail.com>
2016-06-13 11:48:38 +1000

    Resolves #4 - adding more accurate dates for managers

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Single serving website that displays who has a longer current EPL managerial job: Arsene Wenger or every other current EPL manager.
 
+Clarification of the length of other managers EPL careers:
+
+ * Only counting their *current* EPL management period at their *current* club
+   * For managers that got relegated and promoted, only the current season is counted, e.g. Sean Dyche of Burnley
+   * For managers that have switched between EPL clubs only the current club is counted e.g. Mourinho or Koeman
+ * For promoted teams, the start date is ?July 1st?
--- js/main.js
+++ js/main.js
@@ -3,33 +3,90 @@ window.onload = function() {
     var wengers = {
         name: "Arsène Wenger",
         club: "Arsenal",
-        startDate: "22-09-1996",
+        startDate: "01-10-1996",
         daysInCharge: -1, // Calculated later.
         playersAlmostSigned: 1 / 0
     };
 
     // #4 - try and move to external file.
+    // All dates taken from wikipedia - https://en.wikipedia.org/wiki/List_of_Premier_League_managers
     var otherManagers = [
         {
-            name: "José Mourinho",
-            club: "Manchester United",
-            startDate: "27-05-2016",
+            name: "Eddie Howe",
+            club: "AFC Bournemouth",
+            startDate: "12-10-2012"
+        }, {
+            name: "Sean Dyche",
+            club: "Burnley",
+            startDate: "30-10-2012"
+        }, {
+            name: "Guus Hiddink",
+            club: "Chelsea",
+            startDate: "19-12-2015"
+        }, {
+            name: "Alan Pardew",
+            club: "Crystal Palace",
+            startDate: "02-01-2015"
+        }, {
+            name: "David Unsworth", // this is koeman now isnt it?
+            club: "Everton",
+            startDate: "12-05-2016"
+        }, {
+            name: "Steve Bruce",
+            club: "Hull City",
+            startDate: "08-06-2012"
+        }, {
+            name: "Claudio Ranieri",
+            club: "Leicester City",
+            startDate: "13-07-2015"
         }, {
             name: "Jürgen Klopp",
             club: "Liverpool",
-            startDate: "10-11-2015"
+            startDate: "08-10-2015"
         }, {
-            name: "Someøne else",
-            club: "Another club",
-            startDate: "02-03-2001"
+            name: "Manuel Pellegrini", // this is wrong
+            club: "Manchester City",
+            startDate: "14-06-2013"
         }, {
-            name: "Third man",
-            club: "Sample club",
-            startDate: "12-05-2009"
+            name: "José Mourinho",
+            club: "Manchester United",
+            startDate: "27-05-2016"
         }, {
-            name: "Last managêr",
-            club: "LCFC",
+            name: "Aitor Karanka",
+            club: "Middlesbrough",
+            startDate: "13-11-2013"
+        }, {
+            name: "Ronald Koeman",
+            club: "Southampton",
+            startDate: "16-06-2014"
+        }, {
+            name: "Mark Hughes",
+            club: "Stoke City",
+            startDate: "30-05-2013"
+        }, {
+            name: "Sam Allardyce",
+            club: "Sunderland",
+            startDate: "09-10-2015"
+        }, {
+            name: "Francesco Guidolin",
+            club: "Swansea City",
+            startDate: "18-01-2016"
+        }, {
+            name: "Mauricio Pochettino",
+            club: "Tottenham Hotspur",
+            startDate: "27-05-2014"
+        }, {
+            name: "Who is managing them now?", // ???
+            club: "Watford",
             startDate: "02-03-2011"
+        }, {
+            name: "Tony Pulis",
+            club: "West Bromwich Albion",
+            startDate: "01-01-2015"
+        }, {
+            name: "Slaven Bilić",
+            club: "West Ham United",
+            startDate: "09-06-2015"
         }
     ];
 
@@ -45,6 +102,13 @@ window.onload = function() {
     var wengerTotalDaysEmployed = sumEmploymentLengths([wengers]);
     var otherManagersTotalDaysEmployed = sumEmploymentLengths(otherManagers);
     var difference = wengerTotalDaysEmployed - otherManagersTotalDaysEmployed;
+
+    // TODO cleaner way to do this?
+    document.getElementById("otherManagersTable").innerHTML += getRowTemplate()({
+        daysInCharge: otherManagersTotalDaysEmployed,
+        name: 'total',
+        club: ''
+    });
 
     // Display the result
     if (difference >= 0) {
@@ -73,6 +137,8 @@ window.onload = function() {
         for (var i = 0; i < managers.length; ++i) {
             targetElement.innerHTML += template(managers[i]);
         }
+
+        // TODO Also add a total row at the bottom
     }
 
     function getRowTemplate() {
@@ -94,7 +160,7 @@ window.onload = function() {
     }
 
     function daysSince(date) {
-        return moment(date, "DD-MM-YYYY").diff(moment(), 'days')
+        return -moment(date, "DD-MM-YYYY").diff(moment(), 'days')
     }
 
     // Shows images and other html elements associated with a Wenger loss..
